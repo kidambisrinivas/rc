2d2de0bcba8dadd5aeabf1722d3a333e77edf0c9
Srinivas <kidambisrinivas@gmail.com>
2014-07-11 10:13:19 +0000

    Changed shbang to bash for source command support

--- setupHomeFolder.sh
+++ setupHomeFolder.sh
@@ -5,14 +5,14 @@
 
 # 0. Clean messed up installation
 curl https://raw.githubusercontent.com/kidambisrinivas/rc/master/cleanMessedUpInstallation.sh | sh
-exec bash
+source ~/.bashrc
 
 # 1. Ship your own perl
 
 # 1.1 Install perlbrew to manage home folder perls
 curl -kL http://xrl.us/perlbrewinstall | bash
 echo "source ~/perl5/perlbrew/etc/bashrc" >> $HOME/.bashrc
-exec bash
+source ~/.bashrc
 perlbrew available
 
 # 1.2 Install perl 5.18.2 with thread support
